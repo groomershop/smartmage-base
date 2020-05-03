af5dd6b1a9b79bd8e3aae4bbd7feff5b1ff1c8ab
Maciej Tomaszewski <office@groomershop.pl>
2020-05-03 14:47:53 +0200

    Initial commit of SmartMage.pl Base plugin v1.0.1

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Copyright © 2017 SmartMage. All rights reserved.
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Smartmage_Base',
+    __DIR__
+);
